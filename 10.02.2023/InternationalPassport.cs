0af3b14f9b611b71ce265cc4ed9790c66fe2b152
The_Sheleng <sheleng55@gmail.com>
2023-02-10 15:05:00 +0200

    Добавьте файлы проекта.

---
+++ 10.02.2023/InternationalPassport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._02._2023
+{
+    //Задание 1
+    internal class InternationalPassport
+    {
+        int id;
+        string bio;
+        DateTime date;
+        public int CardId { get => id; }
+        public string BIO { get => bio; }
+        public DateTime Date { get => date; }
+        public void Input()
+        {
+            string buffer;
+
+            Console.Write("Card id: ");
+            buffer = Console.ReadLine();
+            if (buffer.Length != 16) throw new Exception("Id must be 16 digits.");
+            id = Convert.ToInt32(buffer);
+
+            Console.Write("BIO: ");
+            buffer = Console.ReadLine();
+            if (buffer.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length != 3)
+                throw new Exception("You must enter your first name, last name and patronymic");
+            bio = buffer;
+
+            Console.Write("Date: ");
+            buffer = Console.ReadLine();
+            date = Convert.ToDateTime(buffer);
+
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Card Id: {CardId}");
+            sb.Append($"BIOL: {BIO}");
+            sb.Append($"Date: {Date.Month}/{Date.Year}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 10.02.2023/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InPass = _10._02._2023.InternationalPassport;
+
+namespace _10._02._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 2
+            try 
+            {
+                Console.Write("Write boolean: ");
+                string str = Console.ReadLine();
+                string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int num1 = int.Parse(words[0]);
+                int num2 = int.Parse(words[2]);
+
+                bool result = false;
+                switch (words[1])
+                {
+                    case "==": result = num1 == num2; break;
+                    case "!=": result = num1 != num2; break;
+                    case ">=": result = num1 >= num2; break;
+                    case "<=": result = num1 <= num2; break;
+                    case ">": result = num1 > num2; break;
+                    case "<": result = num1 < num2; break;
+                    default: throw new Exception("Unknown boolean action");
+                }
+
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
